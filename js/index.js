e07c8ccc707c9687767f3f82e962ea2be446a0e0
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-28 19:43:07 -0500

    paused at like button deliverable

--- js/index.js
+++ js/index.js
@@ -1,32 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
 const showPanelElement = document.getElementById('show-panel')
-
+const thumbnailElement = document.createElement('img')
+ const titleElement = document.createElement('h2')
+ const authorElement = document.createElement('h3')
+const descriptionElement = document.createElement('p')
+const likeButton = document.createElement('button')
+let currentBook
 fetch('http://127.0.0.1:4000/books')
-.then(response => response.json())
-.then(books => {
+    .then(response => response.json())
+    .then(books => {
 
-    books.forEach(book => {
+        books.forEach(book => {
         const liELement = document.createElement('li')
         liELement.textContent = book.title
         const ulElement = document.getElementById('list')
         ulElement.appendChild(liELement)
 
         //adding click event listener to display detail
-        liELement.addEventListener('click', e => {
+            liELement.addEventListener('click', e => {
             displayDetail(book) 
+            })
         })
     })
-})
 
 function displayDetail(book){
-    showPanelElement.innerHTML = ' '
-    const thumbnailElement = document.createElement('img')
-    const titleElement = document.createElement('h2')
-    const authorElement = document.createElement('h3')
-    const descriptionElement = document.createElement('p')
-    const usersList = document.createElement('li')
-    const likeButton = document.createElement('button')
-
+    currentBook = book
     authorElement.textContent = book.author
     titleElement.textContent = book.title
     thumbnailElement.src = book.img_url
@@ -39,6 +37,11 @@ function displayDetail(book){
         usersList.textContent = user.username
         descriptionElement.appendChild(usersList)
         })
-    }
+ }
+//when button clicked, send PATCH request
+likeButton.addEventListener('click', () => {
+    if()
+})
+
 
 });
