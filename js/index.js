3e5d9d3775e7380fe8e7805dbe80068d1162ea78
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-04 23:26:51 -0500

    redo practice, completed all deliverables including patch request

--- js/index.js
+++ js/index.js
@@ -1,163 +1,77 @@
-const ulElement = document.getElementById('list')
-const showPanelElement = document.getElementById('show-panel')
-let userInfo = 
-    { "id": 1992, 
-    "username": "Tkim"}
 
-
-
-    let user = 
-        {"id":1, "username":"pouros"}
-    
+const listUl = document.getElementById('list')
+const divShowPanel = document.getElementById('show-panel')
+let newUser = {"id":7777, "username":"tyler"}
 let currentBook
-
-fetch('http://127.0.0.1:3000/books')
+fetch('http://127.0.0.1:2000/books')
     .then(resp => resp.json())
-    .then(bookArray => {
-        bookArray.forEach(book => {
-            displayTitle(book)
-        })
+    .then(books => {
+        books.forEach(book =>{
+        renderTitle(book)
     })
-//helper function to be used in forEach loop to display each book title on side bar
-function displayTitle(book){
-    const liElement = document.createElement('li')
-    liElement.textContent = book.title
-    ulElement.appendChild(liElement)
+    })
 
-    liElement.addEventListener('click', ()=> {
-        displayDetail(book)
-    })
-}
-//helper function to be used in event handler above. function produces elements, set attrubutes based on fetched data
-function displayDetail(book){
+//render functions
+function renderTitle(book){
+    const li = document.createElement('li')
+    li.textContent = book.title
+    listUl.append(li)
+
+    li.addEventListener('click', () => {
         currentBook = book
-        showPanelElement.innerHTML = ' '// other wise, each details of book gets appended to bottom 
-
-        const imgELement = document.createElement('img')
+        divShowPanel.innerHTML = ' '
+        const imageElement = document.createElement('img')
         const titleElement = document.createElement('h2')
-        const descriptionELement = document.createElement('p')
-        const likeButtonElement = document.createElement('button')
-        const ulForLiElement = document.createElement('ul')
-        imgELement.src = book.img_url
+        const authorElement = document.createElement('h3')
+        const ulELement = document.createElement('ul')
+        const likeButton = document.createElement('button')
+        likeButton.id = 'like'
+
+        imageElement.src = book.img_url
         titleElement.textContent = book.title
-        descriptionELement.textContent = book.description
-        likeButtonElement.textContent = 'LIKE'
-        showPanelElement.append(imgELement, titleElement, descriptionELement, ulForLiElement, likeButtonElement)
+        authorElement.textContent = book.author
+        likeButton.textContent = "LIKE"
+//  
+        let usernameArray = book.users    
+          //make li for users, set its textcontent
 
-        currentBook.users.forEach(user => {   //map or forEach works. there are multiple users who liked single book. need to iterate thrhough
-            const userLikedElement = document.createElement('li')
-            userLikedElement.textContent = user.username
-            ulForLiElement.appendChild(userLikedElement)
-                })
-
-         likeButtonElement.addEventListener('click', ()=> {
-            const userLikedElement = document.createElement('li')  //create if outside of forEach loop. If made in side,
-            currentBook.users.push(user)                       //existing li's are added on top
-            currentBook.users.forEach( user =>{                     //
-                userLikedElement.textContent = user.username
-                ulForLiElement.appendChild(userLikedElement)
-            })
+        usernameArray.forEach(singleUser => {
+            const userLiELement = document.createElement('li')
+            userLiELement.textContent = singleUser.username
+            ulELement.appendChild(userLiELement)
         })
-}  
+        divShowPanel.append(imageElement, titleElement, authorElement,ulELement, likeButton)
+    
 
+        likeButton.addEventListener('click', (e) =>{
+            //get current book's usernamearray - update with new array. create user object
+            let currentArray = currentBook.users
+            
+            currentArray.push(newUser)     //this usernameArray now include new user who liked
+            ulELement.innerHTML = ' '     //clears ul list. then with new user array, runs for each and creates new set of li
+            fetch(`http://127.0.0.1:2000/books/${currentBook.id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(currentBook),
+              }
+            )
+            .then(resp => resp.json())
+            .then(updatedUserLikes => {
+                const updatedUserArray = updatedUserLikes.users
+                updatedUserArray.forEach(user => {
+                const liForNewUser = document.createElement('li')
+                liForNewUser.textContent = user.username
+                ulELement.appendChild(liForNewUser)
+           })
+            })
+
+        })
+    
+    
+    
+    })
 
 
     
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const showPanelElement = document.getElementById('show-panel')
-// const thumbnailElement = document.createElement('img')
-//  const titleElement = document.createElement('h2')
-//  const authorElement = document.createElement('h3')
-// const descriptionElement = document.createElement('p')
-// const likeButton = document.createElement('button')
-// const ulElement = document.getElementById('list')
-// const ulLikesElement = document.createElement('ul')
-
-// console.log(ulElement)
-
-// document.addEventListener("DOMContentLoaded", function() {
-
-// let currentBook
-// const userID = {
-//     id: '1992',
-//     user: "tkim"
-// }
-// fetch('http://127.0.0.1:3000/books')
-//     .then(response => response.json())
-//     .then(books => {
-
-//         books.forEach(book => {
-//             displayTitle(book)
-//         })
-
-//     })
-// function displayTitle(book){
-//     const liELement = document.createElement('li')
-//     liELement.textContent = book.title
-//     ulElement.appendChild(liELement)
-
-//     liELement.addEventListener('click', e => {
-//         displayDetail(book) 
-//     })
-// }
-// function displayDetail(book){
-//     showPanelElement.innerHTML = ' '
-//     currentBook = book
-//     authorElement.textContent = book.author
-//     titleElement.textContent = book.title
-//     thumbnailElement.src = book.img_url
-//     descriptionElement.textContent = book.description
-//     likeButton.textContent = 'Like'
-//     currentBook.users.map(user => {
-//         addUseLikes(user)
-//     })
-//     showPanelElement.append(thumbnailElement, titleElement, authorElement, descriptionElement, likeButton, ulLikesElement)
-
-//     }
-
-// function addUseLikes(user){
-//     const usersList = document.createElement('li')
-//     usersList.textContent = user.username
-//     ulLikesElement.appendChild(usersList)
-    
-// }
-
-// });
-
-
-
-
-
-
-
-// likeButton.addEventListener('click', () => {
-//     currentBook.users.push(userID)
-//     console.log(book.users)
-//     currentBook.users.map(user => {
-
-//         const usersList = document.createElement('li')
-//         usersList.textContent = user.username
-//         descriptionElement.appendChild(usersList)
-//         })
-
-// })
